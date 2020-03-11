bfe9d715f0c1854302abf53b32c2b6a1fe9bf9a5
panosmania <panosmania@users.noreply.github.com>
2020-03-11 13:18:40 +0200

    Updates

---
+++ precache-manifest.a33b13453b2b400e07c87c52668db0ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6df5e6106bc1e6b863019fb2379eee",
+    "url": "/react-robot-cards2-api/index.html"
+  },
+  {
+    "revision": "d322a6c67021c8fb004e",
+    "url": "/react-robot-cards2-api/static/css/main.08b52921.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/react-robot-cards2-api/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-robot-cards2-api/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d322a6c67021c8fb004e",
+    "url": "/react-robot-cards2-api/static/js/main.c6e85214.chunk.js"
+  },
+  {
+    "revision": "d830d7ceb6df56493b61",
+    "url": "/react-robot-cards2-api/static/js/runtime-main.3140c0e7.js"
+  }
+]);
